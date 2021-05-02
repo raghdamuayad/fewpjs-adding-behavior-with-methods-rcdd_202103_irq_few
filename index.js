fdb73c4a9201a6e0b9326402e6b7ed97442889cc
Raghda  Muayad <therose000777@gmail.com>
2021-05-02 18:28:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Your code here
+
+ //class cat {
+  //    constructor(name, sex) {
+    //   this.name = name;
+      //  this.sex = sex;
+
+//        let cat = new Cat("Sasha", "female")
+    //   speak.cat();
+    //}
+
+     //speak() {
+
+
+    //return `${this.name} says meow!`;
+//}
+//console.log(speak.cat());
+  // }
+
+
+//class Dog {
+       //constructor(name, sex) {
+      //   this.name = name;
+         //this.sex = sex;
+      // }
+
+  //newDog() {
+      //  return `${this.name} says woof!`;
+    //  let  dog = newDog("Rufio", "male")
+  //  }
+class Bird {
+  constructor(name,sex) {
+    this.name = name;
+    this.sex = sex;
+  }
+  speak(){
+    if (this.sex === "male") {
+    return "It's me!"+" "+this.name + "," +"the parrot!"
+      const bird = new Bird("Pablo", "male")
+  }else {
+        return this.name + " " + "says" + " " +"squavk!";
+          const  bird2 = new Bird("Mable", "female")
+  }
+  }
+}
